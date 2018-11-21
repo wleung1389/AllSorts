0346d542bb249d4a6d3e1e63367558a75b6d3443
BT_1N3_16 <wleung1389@bths.edu>
2018-11-21 10:10:01 -0500

    hi

--- src/MergeSortLab.java
+++ src/MergeSortLab.java
@@ -1,24 +1,121 @@
 public class MergeSortLab {
-    public void mergeSort(int[] arr) {
+    public void mergeSortInt(int[] arr) {
         int n = arr.length;
         int [] temp = new int[n];
-        mergeSortHelper(arr,0,n - 1, temp);
+        mergeSortHelperInt(arr,0,n - 1, temp);
     }
-    public void mergeSortHelper(int[] arr, int left, int right, int[] temp)
+    public void mergeSortHelperInt(int[] arr, int left, int right, int[] temp)
     {
         if(left < right)
         {
             int mid = (left + right) / 2;
-            mergeSortHelper(arr, left, mid, temp);
-            mergeSortHelper(arr,mid + 1, right, temp);
-            merge(arr,left,mid,right,temp);
+            mergeSortHelperInt(arr, left, mid, temp);
+            mergeSortHelperInt(arr,mid + 1, right, temp);
+            mergeInt(arr,left,mid,right,temp);
         }
     }
-    public void merge(int[] arr, int left, int mid, int right, int[] temp)
+    public void mergeInt(int[] arr, int left, int mid, int right, int[] temp)
     {
-        while(arr[left] < arr[left + 1])
+        for(int x = 0; x < arr.length; x++)
         {
-            
+            temp[x] = arr[x];
+        }
+        int i = left;
+        int j = mid + 1;
+        int k = left;
+        while(i <= mid && j <= right)
+        {
+            if(arr[i] < arr[j])
+            {
+                temp[k] = arr[i];
+                i++;
+            }
+            else
+            {
+                temp[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+        while(i <= mid)
+        {
+            temp[k] = arr[i];
+            i++;
+            k++;
+        }
+        while(j <= right)
+        {
+            temp[k] = arr[j];
+            j++;
+            k++;
+        }
+        for(int z = 0; z < arr.length; z++)
+        {
+            arr[z] = temp[z];
+        }
+    }
+    public void mergeSortStr(String[] arr) {
+        int n = arr.length;
+        String [] temp = new String[n];
+        mergeSortHelperStr(arr,0,n - 1, temp);
+    }
+    public void mergeSortHelperStr(String[] arr, int left, int right, String[] temp)
+    {
+        if(left < right)
+        {
+            int mid = (left + right) / 2;
+            mergeSortHelperStr(arr, left, mid, temp);
+            mergeSortHelperStr(arr,mid + 1, right, temp);
+            mergeStringStr(arr,left,mid,right,temp);
+        }
+    }
+    public void mergeStringStr(String[] arr, int left, int mid, int right, String[] temp)
+    {
+        for(int x = 0; x < arr.length; x++)
+        {
+            temp[x] = arr[x];
+        }
+        int i = left;
+        int j = mid + 1;
+        int k = left;
+        while(i <= mid && j <= right)
+        {
+            int length = 0;
+            if(arr[i].length() > arr[j].length())
+            {
+                length = arr[j].length();
+            }
+            else
+            {
+                length = arr[i].length();
+            }
+            if(arr[i].compareTo(arr[j]) < 0)
+            {
+                temp[k] = arr[i];
+                i++;
+            }
+            else
+            {
+                temp[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+        while(i <= mid)
+        {
+            temp[k] = arr[i];
+            i++;
+            k++;
+        }
+        while(j <= right)
+        {
+            temp[k] = arr[j];
+            j++;
+            k++;
+        }
+        for(int z = 0; z < arr.length; z++)
+        {
+            arr[z] = temp[z];
         }
     }
 }
--- src/Runner.java
+++ src/Runner.java
@@ -2,17 +2,66 @@ public class Runner {
     public static void main(String[] args)
     {
         InPlaceSorts i = new InPlaceSorts();
-        String[] str = i.getRndStringArr(10,10);
-        int[] ints = i.getRndIntArr(1,100,10);
-        double[] doub = i.getRndDoubleArr(1,100,10);
+        MergeSortLab m = new MergeSortLab();
+        String[] str = i.getRndStringArr(1000,10);
+        int[] ints = i.getRndIntArr(1,100,1000000);
+        double[] doub = i.getRndDoubleArr(1,100,50000);
+        int[] ints2 = i.getRndIntArr(1,100,50000);
+        String[] strings = i.getRndStringArr(100000,10);
+
+        System.out.print("Bubble Sort with String Array(Unsorted) : ");
         i.printStrArr(str);
+
+        System.out.print("Insertion Sort with Int Array(Unsorted) : ");
         i.printIntArr(ints);
+
+        System.out.print("Selection Sort with Double Array(Unsorted) : ");
         i.printDoubleArr(doub);
+
+        System.out.print("Merge Sort with Int Array(Unsorted) : ");
+        i.printIntArr(ints2);
+
+        System.out.print("Merge Sort with String Array(Unsorted) : ");
+        i.printStrArr(strings);
+
+        long s = System.nanoTime();
         i.BubbleSort(str);
+        long e = System.nanoTime();
+        System.out.println("Bubble Sort(String) took " + (e-s)/1000000 + " milliseconds.");
+
+        s = System.nanoTime();
         i.insertionSort(ints);
+        e = System.nanoTime();
+        System.out.println("Insertion Sort(Int) took " + (e-s)/1000000 + " milliseconds.");
+
+        s = System.nanoTime();
         i.selectionSort(doub);
+        e = System.nanoTime();
+        System.out.println("Selection Sort(Doubles) took " + (e-s)/1000000 + " milliseconds.");
+
+        s = System.nanoTime();
+        m.mergeSortInt(ints2);
+        e = System.nanoTime();
+        System.out.println("Merge Sort(Int) took " + (e-s)/1000000 + " milliseconds.");
+
+        s = System.nanoTime();
+        m.mergeSortStr(strings);
+        e = System.nanoTime();
+        System.out.println("Merge Sort(String) took " + (e-s)/1000000 + " milliseconds.");
+
+        System.out.print("Bubble Sort with String Array : ");
         i.printStrArr(str);
+
+        System.out.print("Insertion Sort with Int Array : ");
         i.printIntArr(ints);
+
+        System.out.print("Selection Sort with Double Array : ");
         i.printDoubleArr(doub);
+
+        System.out.print("Merge Sort with Int Array : ");
+        i.printIntArr(ints2);
+
+        System.out.print("Merge Sort with String Array : ");
+        i.printStrArr(strings);
     }
 }
